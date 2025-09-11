3c9ec302721434447ffd9984570e4ea586307051
Yehoraz <Yehoraz@users.noreply.github.com>
2025-09-11 03:30:59 +0000

    deploy: 3674bbe51935d52f016aab337abce71c24e49490

--- assets/jszip.min-Rr8aCP3D.js
+++ assets/jszip.min-Rr8aCP3D.js
@@ -1,4 +1,4 @@
-import{g as At,c as bt}from"./index-CJq6-Sub.js";function vt(yt){throw new Error('Could not dynamically require "'+yt+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var St={exports:{}};/*!
+import{g as At,c as bt}from"./index-CNOjWBYb.js";function vt(yt){throw new Error('Could not dynamically require "'+yt+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var St={exports:{}};/*!
 
 JSZip v3.10.1 - A JavaScript class for generating and reading zip files
 <http://stuartk.com/jszip>
